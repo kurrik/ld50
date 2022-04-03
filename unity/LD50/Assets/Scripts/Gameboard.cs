34712e6a05c287fab4d3296c99818aef1fea8363
Arne Roomann-Kurrik <kurrik@gmail.com>
2022-04-02 22:17:39 -0700

    Moving game logic into play state

--- unity/LD50/Assets/Scripts/Gameboard.cs
+++ unity/LD50/Assets/Scripts/Gameboard.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Gameboard : MonoBehaviour {
-  private float _elapsed;
-  private GameCubeCoordinates _coords;
-  private Player _player;
-  private Vector3 _inputDirection;
-
-  public const string TriggerButton = "Trigger";
   public static Gameboard instance = null;
 
-  public Player playerPrefab;
-  public AttackBar attackBar;
-  public PlayerCamera playerCamera;
-  public int gridSize = 30;
-  public float tickTimeStep = 0.5f;
+  public int gridSize = 20;
   public int objectiveRadius = 1;
   public int temporaryBlockageHitPoints = 5;
-  public int spreadBetaHitPoints = 20;
+  public int spreadBetaHitPoints = 5;
 
   public GameStateManager states;
   public StateSplash stateSplash;
@@ -69,81 +59,6 @@ public class Gameboard : MonoBehaviour {
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 
-  private void OnFailedAttack() {
-    Debug.LogFormat("Failed attack!");
-  }
-
-  private void OnAttack() {
-    Debug.LogFormat("Attack!");
-  }
-
-  private void OnAttackPowerUpdate(AttackPowerInfo info) {
-    attackBar.SetValue(info.Amount, info.Color);
-  }
-
-  private void BuildMap() {
-    _coords.Construct(gridSize);
-    /*
-    // Remove 25% of Coordinates except 0,0,0
-    foreach (Vector3 cube in coords.GetCubesFromContainer("all")) {
-      if (cube == Vector3.zero)
-        continue;
-
-      if (Random.Range(0.0f, 100.0f) < 5.0f)
-        coords.RemoveCube(cube);
-    }
-
-    // Remove Coordinates not reachable from 0,0,0
-    coords.RemoveCubes(
-        coords.BooleanDifferenceCubes(
-            coords.GetCubesFromContainer("all"),
-            coords.GetReachableCubes(Vector3.zero, gridSize)
-        )
-    );
-    */
-
-    // Display Coordinates
-    //coords.ShowCoordinatesInContainer("all");
-
-    // Construct Examples
-    ConstructExamples();
-    ConstructPlayer();
-  }
-
-  private void ConstructExamples() {
-    List<Vector3> allCubes = _coords.GetCubesFromContainer("all");
-
-    // Line between the first and last cube coordinate
-    _coords.AddCubesToContainer(_coords.GetLineBetweenTwoCubes(allCubes[0], allCubes[allCubes.Count - 1]), "line");
-
-    // Reachable, 3 coordinates away from 0.0.0
-    _coords.AddCubesToContainer(_coords.GetReachableCubes(Vector3.zero, 3), "reachable");
-
-    // Spiral, 3 coordinates away from 0.0.0
-    _coords.AddCubesToContainer(_coords.GetSpiralCubes(Vector3.zero, 10), "spiral");
-
-    // Path between the first and last cube coordinate
-    _coords.AddCubesToContainer(_coords.GetPathBetweenTwoCubes(allCubes[0], allCubes[allCubes.Count - 1]), "path");
-  }
-
-  private void ConstructPlayer() {
-    Vector3 startingCube = new Vector3(0, 0, 0);
-    _player = Instantiate(playerPrefab, _coords.ConvertCubeToWorldPosition(startingCube), playerPrefab.transform.rotation);
-    _player.Init(startingCube, _coords);
-    playerCamera.Init(_player);
-  }
-
-  private void Tick() {
-    foreach (Vector3 cube in _coords.GetCubesFromContainer("all")) {
-      GameCoordinate coord = _coords.GetCoordinateFromContainer(cube, "all");
-      coord.Tick(cube, _coords);
-    }
-    foreach (Vector3 cube in _coords.GetCubesFromContainer("all")) {
-      GameCoordinate coord = _coords.GetCoordinateFromContainer(cube, "all");
-      coord.ApplyTick();
-    }
-  }
-
   private void Awake() {
     if (instance == null) {
       instance = this;
@@ -151,7 +66,6 @@ public class Gameboard : MonoBehaviour {
       Destroy(gameObject);
       return;
     }
-    _coords = gameObject.GetComponent<GameCubeCoordinates>();
     states = new GameStateManager();
     states.PushState(statePlay);
     if (stateSplash) {
@@ -159,80 +73,11 @@ public class Gameboard : MonoBehaviour {
     }
   }
 
-  private void ShowExample(string key) {
-    _coords.HideCoordinatesInContainer("all");
-    _coords.ShowCoordinatesInContainer(key);
-  }
-
-  void Start() {
-    BuildMap();
-    _elapsed = 0.0f;
-    _player.OnFailedAttack.AddListener(OnFailedAttack);
-    _player.OnAttack.AddListener(OnAttack);
-    _player.OnAttackPowerUpdate.AddListener(OnAttackPowerUpdate);
-  }
-
   void FixedUpdate() {
     states.StateFixedUpdate();
-
-    _elapsed += Time.fixedDeltaTime;
-    if (_elapsed > tickTimeStep) {
-      _elapsed -= tickTimeStep;
-      Tick();
-    }
-    if (_inputDirection.sqrMagnitude > 0) {
-      _player.Move(_inputDirection, _coords);
-    }
   }
 
   void Update() {
     states.StateUpdate();
-
-    if (Gameboard.instance.DebugEnabled) {
-      if (Input.GetKeyDown(KeyCode.F1)) {
-        BuildMap();
-        return;
-      }
-      if (_coords.GetCoordinatesFromContainer("all").Count == 0)
-        return;
-      if (Input.GetKeyDown(KeyCode.F2)) {
-        _coords.ShowCoordinatesInContainer("all");
-      }
-      if (Input.GetKeyDown(KeyCode.F5))
-        ShowExample("line");
-
-      if (Input.GetKeyDown(KeyCode.F6))
-        ShowExample("reachable");
-
-      if (Input.GetKeyDown(KeyCode.F7))
-        ShowExample("spiral");
-
-      if (Input.GetKeyDown(KeyCode.F8))
-        ShowExample("path");
-
-      if (Input.GetKeyDown(KeyCode.F9))
-        Tick();
-
-      if (Input.GetMouseButtonDown(1)) {
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 200f)) {
-          Vector3 cube = _coords.ConvertWorldPositionToCube(hit.point);
-          GameCoordinate coordinate = _coords.GetCoordinateFromContainer(cube, "all");
-          if (coordinate) {
-            coordinate.ClearRadius(cube, _coords, 2);
-          }
-        }
-      }
-    }
-
-    if (Input.GetButtonUp(TriggerButton)) {
-      _player.Trigger(_coords);
-    }
-
-    float horizontalInput = Input.GetAxisRaw("Horizontal");
-    float verticalInput = Input.GetAxisRaw("Vertical");
-    _inputDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
   }
 }
--- unity/LD50/Assets/Scripts/StatePlay.cs
+++ unity/LD50/Assets/Scripts/StatePlay.cs
@@ -14,12 +14,17 @@ public struct LevelInfo {
 
 public class StatePlay : GameStateMonoBehavior {
   public const string MenuButton = "Menu";
+  public const string TriggerButton = "Trigger";
   public HUD HUD;
+  public float tickTimeStep = 0.5f;
 
   public StateMenu stateMenu;
   public StateLevelComplete stateLevelComplete;
   public StateTerminal stateGameOver;
   public StateTerminal stateGameWon;
+  public Player playerPrefab;
+  public AttackBar attackBar;
+  public PlayerCamera playerCamera;
 
   private int currentLevel = 0;
   private LevelInfo[] levels = {
@@ -32,11 +37,69 @@ public class StatePlay : GameStateMonoBehavior {
   };
   private LevelInfo level { get => levels[currentLevel]; }
 
+  private float _elapsed;
+  private GameCubeCoordinates _coords;
+  private Player _player;
+  private Vector3 _inputDirection;
+
+  private void Awake() {
+
+  }
+
   private void Start() {
+    _elapsed = 0.0f;
+    _coords = gameObject.GetComponent<GameCubeCoordinates>();
     stateMenu.gameObject.SetActive(false);
     stateLevelComplete.gameObject.SetActive(false);
     stateGameOver.gameObject.SetActive(false);
     stateGameWon.gameObject.SetActive(false);
+    LoadLevel();
+  }
+
+  private void LoadLevel() {
+    _coords.Construct(20);
+    LoadPlayer();
+  }
+
+  private void LoadPlayer() {
+    Vector3 startingCube = new Vector3(0, 0, 0);
+    _player = Instantiate(playerPrefab, _coords.ConvertCubeToWorldPosition(startingCube), playerPrefab.transform.rotation);
+    _player.Init(startingCube, _coords);
+    playerCamera.Init(_player);
+    _player.OnFailedAttack.AddListener(OnFailedAttack);
+    _player.OnAttack.AddListener(OnAttack);
+    _player.OnAttackPowerUpdate.AddListener(OnAttackPowerUpdate);
+  }
+
+  private void Tick() {
+    foreach (Vector3 cube in _coords.GetCubesFromContainer("all")) {
+      GameCoordinate coord = _coords.GetCoordinateFromContainer(cube, "all");
+      coord.Tick(cube, _coords);
+    }
+    foreach (Vector3 cube in _coords.GetCubesFromContainer("all")) {
+      GameCoordinate coord = _coords.GetCoordinateFromContainer(cube, "all");
+      coord.ApplyTick();
+    }
+  }
+
+  private void OnFailedAttack() {
+    Debug.LogFormat("Failed attack!");
+  }
+
+  private void OnAttack() {
+    Debug.LogFormat("Attack!");
+  }
+
+  private void OnAttackPowerUpdate(AttackPowerInfo info) {
+    attackBar.SetValue(info.Amount, info.Color);
+  }
+
+  private void SetGameState(GameStateMonoBehavior state) {
+    if (stateManager != null) {
+      state.gameObject.SetActive(true);
+      Debug.LogFormat("Menu enabled");
+      stateManager.PushState(state);
+    }
   }
 
   public override void OnCurrentEnter() {
@@ -52,16 +115,42 @@ public class StatePlay : GameStateMonoBehavior {
       stateMenu.SetText("Neat");
       SetGameState(stateMenu);
     }
+    if (Gameboard.instance.DebugEnabled) {
+      if (Input.GetKeyDown(KeyCode.F1)) {
+        LoadLevel();
+        return;
+      }
+      if (Input.GetMouseButtonDown(1)) {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 200f)) {
+          Vector3 cube = _coords.ConvertWorldPositionToCube(hit.point);
+          GameCoordinate coordinate = _coords.GetCoordinateFromContainer(cube, "all");
+          if (coordinate) {
+            coordinate.ClearRadius(cube, _coords, 2);
+          }
+        }
+      }
+    }
+
+    if (Input.GetButtonUp(TriggerButton)) {
+      _player.Trigger(_coords);
+    }
+
+    float horizontalInput = Input.GetAxisRaw("Horizontal");
+    float verticalInput = Input.GetAxisRaw("Vertical");
+    _inputDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
   }
 
   public override void StateFixedUpdate(GameStateManager states) {
-  }
-
-  private void SetGameState(GameStateMonoBehavior state) {
-    if (stateManager != null) {
-      state.gameObject.SetActive(true);
-      Debug.LogFormat("Menu enabled");
-      stateManager.PushState(state);
+    _elapsed += Time.fixedDeltaTime;
+    if (_elapsed > tickTimeStep) {
+      _elapsed -= tickTimeStep;
+      Tick();
+    }
+    if (_inputDirection.sqrMagnitude > 0) {
+      _player.Move(_inputDirection, _coords);
     }
   }
 }
