d61c8913e615310a6d485167a9527bab525fdc7e
Arne Roomann-Kurrik <kurrik@gmail.com>
2022-04-04 10:44:43 -0700

    Fix menus to work with gamepads

--- unity/LD50/Assets/Scripts/StateLevelComplete.cs
+++ unity/LD50/Assets/Scripts/StateLevelComplete.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class StateLevelComplete : GameStateMonoBehavior {
   public const string AdvanceButton = "Trigger";
   public TMPro.TextMeshProUGUI textBox;
+  public float minTimeSeconds = 1.0f;
+  private float menuLoadedTime;
+  private bool exit;
 
   public void SetText(string text) {
     textBox.text = text;
@@ -12,10 +15,16 @@ public class StateLevelComplete : GameStateMonoBehavior {
 
   public override void OnCurrentEnter() {
     Time.timeScale = 0.0f;
+    menuLoadedTime = Time.realtimeSinceStartup;
+    exit = false;
   }
 
   public override void StateUpdate(GameStateManager states) {
     if (Input.GetButtonUp(AdvanceButton)) {
+      exit = true;
+    }
+    float elapsed = Time.realtimeSinceStartup - menuLoadedTime;
+    if (exit && elapsed >= minTimeSeconds) {
       states.PopState();
       gameObject.SetActive(false);
       Time.timeScale = 1.0f;
